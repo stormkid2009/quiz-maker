eb5b4e0bd9252b64e0c000e79fcea982209d50ec
Anwar Ahmed <nileking2007@gmail.com>
2025-04-16 22:06:27 +0200

    chore: add resuable boilerplate

--- src/appXX/exam/server/passageServerComponent.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Passage(){
-    return(
-        <div>
-            <h1>Passage</h1>
-        </div>
-    )
-}
--- src/appXX/server/grammaire/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Grammaire(){
-    return(
-        <div>
-            <h1>Grammaire</h1>
-        </div>
-    )
-}
