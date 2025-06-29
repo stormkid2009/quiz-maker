7fc97294f00255979d82b2630c17c84f074127d4
Anwar Ahmed <nileking2007@gmail.com>
2025-06-30 00:10:48 +0300

    feat: add documentation to answer input  component

--- src/components/inputs/answer.tsx
+++ src/components/inputs/answer.tsx
@@ -2,6 +2,15 @@
 
 import React from "react";
 
+/**
+ * Props for the Answer component, representing a single answer option.
+ *
+ * @interface AnswerOption
+ * @property {number} index - Index of the option.
+ * @property {string} content - The text content of the option.
+ * @property {boolean} checked - Whether the option is currently selected.
+ * @property {(newChecked: boolean, index: number) => void} onToggle - Callback invoked when the option is toggled.
+ */
 interface AnswerOption {
   index: number;
   content: string;
@@ -9,8 +18,20 @@ interface AnswerOption {
   onToggle: (newChecked: boolean, index: number) => void;
 }
 
+/**
+ * Answer component renders a memoized checkbox option for a question.
+ *
+ * @param {AnswerOption} props - Props for the answer option.
+ * @returns {JSX.Element} The rendered answer option element.
+ */
 const Answer: React.FC<AnswerOption> = React.memo(
   ({ index, content, checked, onToggle }) => {
+    /**
+     * Handles change events for the checkbox input.
+     *
+     * @param {React.ChangeEvent<HTMLInputElement>} e - The change event.
+     * @returns {void}
+     */
     const handleChange = React.useCallback(
       (e: React.ChangeEvent<HTMLInputElement>) => {
         onToggle(e.target.checked, index);
