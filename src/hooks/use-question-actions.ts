6bc965d957f38c53d24e84a78c08af54b31f28a4
Anwar Ahmed <nileking2007@gmail.com>
2025-06-29 22:51:08 +0300

    feat: add documentation to use question actions hook

--- src/hooks/use-question-actions.ts
+++ src/hooks/use-question-actions.ts
@@ -1,11 +1,24 @@
 import { useCallback } from "react";
 
+/**
+ * Options for the useQuestionActions hook.
+ * @property {() => void} onShowAnswer - Callback invoked when the "show answer" action is triggered.
+ * @property {() => void} onLoadNew - Callback invoked when the "load new question" action is triggered.
+ * @property {string} [customEventName="showAnswer"] - Name of the custom event dispatched when showing the answer.
+ */
 interface UseQuestionActionsOptions {
   onShowAnswer?: () => void;
   onLoadNew?: () => void;
   customEventName?: string;
 }
 
+/**
+ * Return value of the useQuestionActions hook.
+ * @property {() => void} handleShowAnswer - Function to trigger the "show answer" action.
+ * @property {() => void} handleLoadNew - Function to trigger the "load new question" action.
+ * @property {() => void} showAnswer - Alias for handleShowAnswer.
+ * @property {() => void} loadNewQuestion - Alias for handleLoadNew.
+ */
 interface UseQuestionActionsReturn {
   handleShowAnswer: () => void;
   handleLoadNew: () => void;
@@ -13,8 +26,14 @@ interface UseQuestionActionsReturn {
   loadNewQuestion: () => void;
 }
 
+/**
+ * React hook to provide handlers for showing answers and loading new questions.
+ *
+ * @param {UseQuestionActionsOptions} [options={}] - Configuration options for the hook.
+ * @returns {UseQuestionActionsReturn} An object containing action handler functions.
+ */
 export function useQuestionActions(
-  options: UseQuestionActionsOptions = {},
+  options: UseQuestionActionsOptions = {}
 ): UseQuestionActionsReturn {
   const { onShowAnswer, onLoadNew, customEventName = "showAnswer" } = options;
 
